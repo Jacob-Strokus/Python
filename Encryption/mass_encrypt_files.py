e35fe08cc9ab3d8455723b5fc6ee2c018477d939
Jacob Strokus <jasstrokus8@gmail.com>
2020-10-08 22:14:46 -0400

    Update mass_encrypt_files.py
    mass file encrypter

--- Encryption/mass_encrypt_files.py
+++ Encryption/mass_encrypt_files.py
@@ -2,7 +2,7 @@
 # Jacob Strokus
 
 # Import statements
-import shutil, os, errno
+import shutil, os, errno, time
 
 # encrypt the files.
 def encrypt(item):
@@ -13,16 +13,22 @@ def encrypt(item):
 def main():
 
     files_in_dir = []
+    dirs = []
     source = input(r'Where should I start: ')
     
     # r=>root, d=>directories, f=>files
     for r, d, f in os.walk(source):
+
+        for item in d:
+            dirs.append(os.path.join(r, item))
         for item in f:
             files_in_dir.append(os.path.join(r, item))
+            
 
-    print('Encrypting:\n')
-    for dirs in d:
-        print('\\' + dirs + '\n')
+    print('Encrypting:\n') # this block of code is only used for visual. Can be removed.
+    for d in dirs:
+        time.sleep(2) # Easier on the eyes but can removed for faster encrypting
+        print('\\' + d + '\n')
             
 
     for item in files_in_dir:
